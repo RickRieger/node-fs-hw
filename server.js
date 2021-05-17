b71779d9353e6cf178ff90a873fa1f6283018805
Rick Rieger <rickrieger36251@gmail.com>
2021-05-17 08:18:57 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const http = require("http");
+const fs = require('fs');
+const path = require('path');
+
+http
+.createServer(function (request, response) {
+  if (request.url === "/create-directory") {
+    fs.mkdir("content", function (error, data) {
+      if (error) {
+        response.end(err);
+      } else {
+        response.end("content folder created");
+      }
+    });
+  } 
+  if (request.url === "/create-text" && request.method === "POST") {
+    let body = "";
+    request.on("data", function (data) {
+      body += data.toString();
+    });
+    request.on("end", function () {
+      let parsedBody = JSON.parse(body);
+      fs.writeFile("randomText.txt", "This is some foreign stuff, but I'm gonna get it, and make some money!", function (err) {
+        if (err) {
+          response.end(err);
+        } else {
+          response.end("randomText.txt created");
+        }
+      });
+    });
+  }
+  if (request.url === "/new-folder-and-file" && request.method === "POST") {
+    let body = "";
+    request.on("data", function (data) {
+      body += data.toString();
+    });
+    request.on("end", function () {
+      let parsedBody = JSON.parse(body);
+      fs.readFile('randomText.txt', 'utf8' , (err, data) => {
+        if (err) {
+          response.error(error)
+          return
+        }
+        
+        fs.writeFile("content/verbage.txt",data, function (err) {
+          if (err) {
+            response.end(err);
+          } else {
+            response.end("verbage.txt created");
+          }
+        });
+      });
+    });
+    setTimeout(function(){
+      fs.unlink("./content/verbage.txt", ()=>{
+        fs.rmdir("content", ()=>(console.log("content removed")));
+      }) }, 7000);
+    }
+  })
+  
+  .listen(3000, function () {
+    console.log("Server Started!!!");
+  });
